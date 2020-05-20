eef371605e88f783c7edb5719362718db69dba89
Vivek <vivekannie@yahoo.com>
2020-05-20 06:59:20 -0400

    final dice

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,51 @@
-function dise() {
-    var roll = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("history").innerHTML += " " + roll + "/6";
-    if (roll === 6) {
-      document.getElementById("rollResult").innerHTML = "dise roll: Wow 6!!";
-    } else if (roll === 1) {
-      document.getElementById("rollResult").innerHTML = "dise roll: good!!";
-    } else {
-        console.log()
-      document.getElementById("rollResult").innerHTML = "dise roll: " + roll;}
-  }
+const dice = document.querySelector('#dice')
+const roll = document.querySelector('#roll')
+const total = document.querySelector('#total-number')
+const showAll = document.querySelector('#total-rolls')
+const list = document.querySelector('#list')
+
+let totalRolls = 0
+let diceIndex = 0
+let dieRolls = []
+let reset = document.querySelector('#reset')
+var sound = new Audio();
+sound.src ='https://www.myinstants.com/media/sounds/shake-and-roll-dice-soundbible.mp3'
+
+roll.addEventListener('click', function(){
+
+
+    while (diceIndex < dice.value) {
+    let randomRoll = Math.floor(Math.random() * 6) + 1
+    totalRolls += randomRoll
+
+    console.log (randomRoll)
+    diceIndex += 1
+    dieRolls.push(randomRoll)
+}
+total.innerText = 'Total:' +  totalRolls
+
+console.log ( dieRolls)
+sound.play()
+})
+
+showAll.addEventListener ('click', function(){
+
+ while (dieRolls.length > 0){
+     const listItems = document.createElement('li')
+     listItems.innerHTML =+ dieRolls.pop([0])
+     list.appendChild(listItems)
+     diceIndex += 1
+ }
+})
+
+reset.addEventListener ('click', function(){
+    dieRolls = []
+    dice.value = ''
+    total.innerHTML = ''
+    totalRolls = 0
+    list.innerHTML = ''
+    diceIndex = 0
+    
+    } )
+    
+
